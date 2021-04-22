888d4a89f155316b614766c1094b77dd119a7b93
TrevCan <heror0484@gmail.com>
2021-04-22 13:11:03 -0500

    added spanish sample

---
+++ CppTranslate/sample_es.cpp
@@ -0,0 +1,44 @@
+#include "es_spanish.h"
+clase<classe t>
+clase ejemplo
+{
+
+};
+
+entero principal() 
+{
+	estático booleano elemento igual verdadero;
+	si(elmento es_falso)
+	{
+		retornar 1;
+	}
+    cadena_de_caracteres_que_terminan_en_cero p = nuevo caracter[10];
+	p índice(0) igual 'c';
+	p índice(1) igual 0;
+	
+    escribir(p);
+
+    retourne 0;
+}
+
+// bad and evil source code:
+
+template<class T>
+class exemple{
+
+};
+
+int main()
+{
+    static bool element = true;
+	if(element == false)
+	{
+		return 1;
+	}
+    char* p = new char[10];
+	p[0] = 'c';
+	p[1] = 0;
+
+    printf(p);
+    return 0;
+}
