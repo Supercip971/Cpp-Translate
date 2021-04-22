06f3a19bb37c33067869d7e68753f0cd52a31a5a
supercyp <supercyp@protonmail.com>
2021-04-22 07:25:31 -0400

    french: it's now better and yes the joke is not over >:)

--- CppTranslate/fr_french.h
+++ CppTranslate/fr_french.h
@@ -3,6 +3,8 @@
 // juste pour le fun
 
 #include <string>
+#include <list>
+#include <cstring>
 #include <vector>
 #define cpp_français
 #pragma region autre
@@ -14,26 +16,35 @@
 #pragma endregion
 
 #pragma region operateur
-#define classe class
+
+#define nom_de_type typename
 #define modèle template
-#define constant const
-#define taille_de size_of
+#define taille_de sizeof
 #define opérateur operator
+
+#define classe class
 #define structure struct
 #define énumération enum
+
+#define constant const
 #define virtuel virtual
 #define statique static
+#define sur_la_ligne inline
+
 #define publique public
 #define privé private
 #define sécurisé protected
-#define nom_de_type typename
+
 #define identificateur_de_type typeid
+
 #define envoyer throw
-#define retourner return
+#define retourne return
+
 #define nouveau new
 #define nouvelle new
 #define supprimer delete
 #define supprimer_tableaux delete[]
+
 #define ou or
 #define et and
 #define non not
@@ -46,12 +57,19 @@
 #define multiplier *
 #define soustraire -
 #define diviser /
-#define addresse_mémoire(A) &A
+
+#define référence_de(A) (&A)
 #define pointeur(A) A*
-#define enfant :
+#define enfant_de :
+
+#define plus_grand_que >
+#define plus_petit_que <
+#define plus_grand_ou_égal >=
+#define plus_petit_ou_égal <=
 
 #define est_vrai == vrai
 #define est_faux == faux
+
 #pragma endregion
 
 #pragma region type
@@ -64,13 +82,21 @@ utilisant intégral = int;
 utilisant flottant = float;
 utilisant double_flottant = double;
 utilisant phrase = std::string;
+
+utilisant chaine_de_caractère_c_terminant_par_un_zero = const char*;
+
 modèle<classe T>
 utilisant vecteur = std::vector < T>;
 
+modèle<classe T>
+utilisant liste = std::list<T>;
+
 #pragma endregion
 #pragma region boucle_ou_autre
+
 #define si if
 #define sinon else
+#define sinon_si else if
 #define répéter_tant_que while
 #define pour for
 #define pour_chacun for each
@@ -80,3 +106,8 @@ utilisant vecteur = std::vector < T>;
 #define écrire printf
 
 #pragma endregion
+
+#define copier_mémoire memcpy
+#define mettre_mémoire memset
+#define taille_dune_chaine_de_caractère_c strlen
+#define copier_chaine_de_caractère strcpy
