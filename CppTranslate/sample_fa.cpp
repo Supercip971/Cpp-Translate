381a3de2dfaab2103dfacc6b913c204517b750b3
esdmr <esdmr0@gmail.com>
2021-04-23 13:23:26 +0430

    Add Persian (fa_IR) language

---
+++ CppTranslate/sample_fa.cpp
@@ -0,0 +1,44 @@
+#include "fa_persian.h"
+قالب<کلاس ت>
+کلاس مثال
+{
+
+};
+
+صحیح نقطه‌ورودی()
+{
+	ساکن بولی عنصر می‌شود صحیح;
+	اگر(عنصر غلط‌است)
+	{
+		برگردان 1 را;
+	}
+    رشته‌کارکترهای‌سی‌که‌باصفر‌ختم‌می‌شود پ می‌شود نو کارکتر[10];
+	پ اندیس(0) می‌شود 'c';
+	پ اندیس(1) می‌شود 0;
+
+    بنویس(پ) را;
+
+    برگردان 0 را;
+}
+
+// bad and evil source code:
+
+template<class T>
+class exemple{
+
+};
+
+int main()
+{
+    static bool element = true;
+	if(element == false)
+	{
+		return 1;
+	}
+    char* p = new char[10];
+	p[0] = 'c';
+	p[1] = 0;
+
+    printf(p);
+    return 0;
+}
