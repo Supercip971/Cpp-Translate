87726567935ff2bd4cd11bb5f06f85cd4c03a024
3nt3 <gott@3nt3.de>
2021-04-24 18:30:17 +0200

    run clang-format

--- CppTranslate/sample_de.cpp
+++ CppTranslate/sample_de.cpp
@@ -1,40 +1,33 @@
 #include "de_german.h"
-vorlage<klasse t>
-klasse Beispiel {
+vorlage<klasse t> klasse Beispiel{
 
 };
 
 ganzzahl haupt() {
-    statisch bool element gleich wahr;
-    falls (element ist_falsch) {
-            rückgabe 1;
-    }
-    c_zeichenkette p = neu zeichen[10];
-    p[0] = 'c';
-    p[1] = 0;
-	
-    ausgabef(p);
+  statisch bool element gleich wahr;
+  falls(element ist_falsch) { rückgabe 1; }
+  c_zeichenkette p = neu zeichen[10];
+  p[0] = 'c';
+  p[1] = 0;
 
-    rückgabe 0;
+  ausgabef(p);
+
+  rückgabe 0;
 }
 
 // bad and evil source code:
 
-template<class T>
-class Example {
-
-};
+template <class T> class Example {};
 
 int main() {
-    static bool element = true;
-	if(element == false)
-	{
-		return 1;
-	}
-    char* p = new char[10];
-	p[0] = 'c';
-	p[1] = 0;
+  static bool element = true;
+  if (element == false) {
+    return 1;
+  }
+  char *p = new char[10];
+  p[0] = 'c';
+  p[1] = 0;
 
-    printf(p);
-    return 0;
+  printf(p);
+  return 0;
 }
