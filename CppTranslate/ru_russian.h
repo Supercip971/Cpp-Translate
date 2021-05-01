92047629a86c97ffe6f8927c1b09b2edbad42541
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-05-01 19:43:28 +0430

    support russian
    Russian is one of UN languages

---
+++ CppTranslate/ru_russian.h
@@ -0,0 +1,12 @@
+#pragma once
+// c++ на русском
+// просто для развлечения
+
+#include <string>
+#include <list>
+#include <cstring>
+#include vector
+
+#define с_использованием using
+
+#define основной main
