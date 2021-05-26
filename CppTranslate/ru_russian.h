234e072214c2172c9cee2598d61caba5f747b0aa
PooiaFerdowsi <pooia.ferdowsi.is.developer@gmail.com>
2021-05-26 17:46:52 +0430

    a mistake & support more words (#26)
    * a mistake & support more words
    
    the `vector` replaced by `<vector>`
    and add the fayl and liniya words
    
    * Update ru_russian.h
    
    add the yesli word
    
    * Update ru_russian.h
    
    add else
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h
    
    * Update ru_russian.h

--- CppTranslate/ru_russian.h
+++ CppTranslate/ru_russian.h
@@ -5,8 +5,21 @@
 #include <string>
 #include <list>
 #include <cstring>
-#include vector
+#include <vector>
 
 #define с_использованием using
 
 #define основной main
+
+#define линия __LINE__
+#define файл __FILE__
+
+#define если if
+#define еще else
+
+#define правда true
+#define ложный false
+
+#define и and
+#define или or
+#define не not
