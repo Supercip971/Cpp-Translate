dfce35e90fa057c56b62c5c68f8a431f76a9b602
krzysckh <76700379+krzysckh@users.noreply.github.com>
2021-06-09 23:40:21 +0200

    polski dodano lmao (#27)

---
+++ CppTranslate/sample_pl.cpp
@@ -0,0 +1,34 @@
+/*
+ * dosłownie najgorszy kod jaki napisałem kiedykolwiek
+ * literally the worst piece of code i have ever written
+ */
+
+
+#include "pl_polish.h"
+#include <iostream>
+
+używając namespace std;
+
+liczba główna() {
+
+	ciąg_znaków ilośćPapieży_ciąg_znaków;
+	
+	wypisz << "ile chciałbyś papieży? \n";
+	wejście >> ilośćPapieży_ciąg_znaków;
+
+	liczba ilośćPapieży równe stoi(ilośćPapieży_ciąg_znaków);
+
+	
+
+	liczba papież równe 0;
+	dopóki (papież mniejsze_niż ilośćPapieży) {
+		wypisz << "papieżak " << papież << endl;
+		papież równe papież dodaj 1;
+	}
+
+	jeżeli (ilośćPapieży czy_równe 2137) {
+		wypisz << "gratulacje! jesteś koneserem prawdziwego humoru B)\n";
+	}
+
+	oddaj 0;
+}
