11461d2c925a17a2eb77b1c7dad306871ce2b26a
krzysckh <krzysckh@wp.pl>
2021-06-09 19:58:59 +0200

    polski dodano lmao

---
+++ CppTranslate/pl_polish.h
@@ -0,0 +1,63 @@
+#include <string>
+#include <list>
+#include <cstring>
+#include <iostream>
+
+
+#define używając using
+#define główna main
+#define __LINIA__ __LINE__
+#define __PLIK__ __FILE__
+
+#define szablon template
+
+#define wielkość sizeof
+#define klasa class
+#define struktura struct
+#define stała const
+#define wirtualna virtual
+#define publiczna public
+#define chroniona protected
+
+#define rzuć throw
+#define oddaj return
+
+#define nowy new
+#define usuń delete
+#define usuń_tablicę delete[]
+
+#define lub or
+#define oraz and
+#define nie not
+
+#define prawda true
+#define fałsz false
+
+#define dodaj +
+#define równe =
+#define czy_równe ==
+#define pomnóż *
+#define odejmij -
+#define podziel /
+
+#define większe_niż >
+#define mniejsze_niż <
+
+używając pusta = void;
+
+używając znak = char;
+używając liczba = int;
+używając liczba_przecinkowa = float;
+używając podwójna = double;
+używając ciąg_znaków = std::string;
+
+używając c_ciąg_znaków = char*;
+
+#define jeżeli if
+#define jaknieto else
+#define jaknieto_jeżeli else if
+#define dopóki while
+#define dla for
+
+#define wypisz cout
+#define wejście cin
